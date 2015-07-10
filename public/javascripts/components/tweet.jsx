8a4fb62b799f0a125510c06705996a63843c6cdb
Daniel Howard <DH@Daniels-MacBook-Air.local>
2015-07-09 19:03:25 -0700

    Handle photos in tweets

--- public/javascripts/components/tweet.jsx
+++ public/javascripts/components/tweet.jsx
@@ -1,5 +1,23 @@
 var Tweet = React.createClass({
 
+  renderImages: function(tweet) {
+    if(!tweet.entities.media) {
+      return null;
+    }
+
+    return (
+      <div className="image-holder">
+        tweet.entities.media.map( function(media){
+          return (
+            <a href={ media[0].display_url }>
+              <img src={ media[0].media_url } />
+            </a>
+          );
+        })
+      </div>
+    )
+  },
+
   render: function() {
     var tweet = this.props.tweet;
 
@@ -13,6 +31,8 @@ var Tweet = React.createClass({
         <p className="content">
           { tweet.text }
         </p>
+
+        { this.renderImages(tweet) }
 
       </div>
     )
--- public/javascripts/components_build/tweet.js
+++ public/javascripts/components_build/tweet.js
@@ -1,5 +1,21 @@
 var Tweet = React.createClass({displayName: "Tweet",
 
+  renderImages: function(tweet) {
+    if(!tweet.entities.media) {
+      return null;
+    }
+
+    return (
+      React.createElement("div", {className: "image-holder"}, 
+         (tweet.entities.media) ?
+          React.createElement("a", {href: tweet.entities.media[0].display_url}, 
+            React.createElement("img", {src: tweet.entities.media[0].media_url})
+          ) : null
+        
+      )
+    )
+  },
+
   render: function() {
     var tweet = this.props.tweet;
 
@@ -12,7 +28,9 @@ var Tweet = React.createClass({displayName: "Tweet",
 
         React.createElement("p", {className: "content"}, 
            tweet.text
-        )
+        ), 
+
+         this.renderImages(tweet) 
 
       )
     )
